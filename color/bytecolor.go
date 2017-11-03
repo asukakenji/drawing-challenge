083d18f2c49cf894699dbeae6b037eedd34c21ab
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2017-11-03 11:04:39 +0800

    Reorganized the source code

--- color/bytecolor.go
+++ color/bytecolor.go
@@ -2,15 +2,6 @@ package color
 
 import "github.com/asukakenji/drawing-challenge/common"
 
-// ByteColor represents a color value using a byte.
-// It implements the Color interface.
-type ByteColor byte
-
-// Ensure that ByteColor implements the Color interface.
-var (
-	_ Color = ByteColor(0)
-)
-
 // ParseByteColor returns a ByteColor from a single-byte string.
 //
 // Errors
@@ -29,13 +20,4 @@ func ParseByteColor(s string) (ByteColor, error) {
 		return ByteColor(0), common.ErrInvalidColor
 	}
 	return ByteColor(s[0]), nil
-}
-
-// Equals returns whether this Color equals c.
-func (bc ByteColor) Equals(c Color) bool {
-	bc2, ok := c.(ByteColor)
-	if !ok {
-		return false
-	}
-	return bc == bc2
 }
--- color/color.go
+++ color/color.go
@@ -2,9 +2,27 @@ package color
 
 // Color represents a color value.
 //
-// There is no requirement on the underlying implementation as long as
-// it is supported by the canvas which uses it.
+// There is no requirement on the underlying implementation
+// as long as it is supported by the canvas which uses it.
 type Color interface {
 	// Equals returns whether this Color equals c.
 	Equals(c Color) bool
+}
+
+// ByteColor represents a color value using a byte.
+// It implements the Color interface.
+type ByteColor byte
+
+// Ensure that ByteColor implements the Color interface.
+var (
+	_ Color = ByteColor(0)
+)
+
+// Equals returns whether this Color equals c.
+func (bc ByteColor) Equals(c Color) bool {
+	bc2, ok := c.(ByteColor)
+	if !ok {
+		return false
+	}
+	return bc == bc2
 }
